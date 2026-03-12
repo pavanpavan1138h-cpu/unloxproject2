1cd92644a6d1ef2b45d270e95ea0b96312f57e87
Pavan R <pavanpavan1138h@gmail.com>
2026-03-12 22:04:57 +0530

    Fix dotenv and mongoose connection setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 dotenv.config();
 
