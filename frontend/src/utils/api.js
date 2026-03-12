376faa95e7f28ed5ce8d12dbc4fa4a25f445324b
pavanpavan1138h-cpu <pavanpavan1138h@gmail.com>
2026-03-12 22:44:28 +0530

    api url added

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-});
+const api = axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/users`)
 
 // Add a request interceptor
 api.interceptors.request.use(
