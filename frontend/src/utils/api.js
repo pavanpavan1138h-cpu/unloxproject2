97f04d784e7715ea01e549c6d49db58a05ccb8ce
Pavan R <pavanpavan1138h@gmail.com>
2026-03-12 23:05:29 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,19 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/users`)
+const api = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+});
 
 // Add a request interceptor
 api.interceptors.request.use(
-    (config) => {
-        const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
-        if (token) {
-            config.headers['x-auth-token'] = token;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        config.headers["x-auth-token"] = token;
+      }
     }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
